bc922714d03c0ef1e6162ac374756ddec506e8fa
!ereHyddeS <seanespiritu615@gmail.com>
2021-08-13 17:55:07 +0800

    visible pain 2

--- scripts/libs/statuses.js
+++ scripts/libs/statuses.js
@@ -1,10 +1,7 @@
 //referencing pixelcraft's format
 //thera status effect
-const theraStatus = extend(StatusEffect, "thera-status", {
-    isHidden(){
-        return false
-    },
-    localizedName: "Thera",
+const theraStatus = extend(StatusEffect, "theraStatus", {
+    localizedName: "Thera Faction",
     speedMultiplier: 1.2,
     healthMultiplier: 1.4
     damage: -10
--- scripts/units/theraFaction.js
+++ scripts/units/theraFaction.js
@@ -1,3 +1,13 @@
 const statuses = require("libs/statuses")
 
-thera.abilities.add(new StatusFieldAbility(theraStatus, 300, 300, 80));
+//thera status effect
+const theraStatus = extend(StatusEffect, "theraStatus", {
+    localizedName: "Thera Faction",
+    speedMultiplier: 1.2,
+    healthMultiplier: 1.4
+    damage: -10
+});
+
+Events.on(ClientLoadEvent, b  => {
+   thera.abilities.add(new StatusFieldAbility(theraStatus, 300, 300, 80));
+});
