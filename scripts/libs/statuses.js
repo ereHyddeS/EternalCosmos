ebc878df1c6cd54cf3914d8e8b72299030d9f2f5
!ereHyddeS <seanespiritu615@gmail.com>
2021-08-14 09:36:52 +0800

    Thera Balancing

--- scripts/libs/statuses.js
+++ scripts/libs/statuses.js
@@ -4,6 +4,7 @@ const theraStatus = extend(StatusEffect, "theraStatus", {
     localizedName: "Thera Faction",
     speedMultiplier: 1.2,
     healthMultiplier: 1.4,
+    buildSpeedMultiplier: 1.5
     damage: -0.4
 });
 
--- scripts/units/theraFaction.js
+++ scripts/units/theraFaction.js
@@ -5,5 +5,5 @@ const thera = extendContent(UnitType, "thera", {});
 thera.constructor = () => extend(UnitEntity, {
 });
 Events.on(ClientLoadEvent, b  => {
-thera.abilities.add(new StatusFieldAbility(statuses.theraStatus, 300, 300, 80));
+thera.abilities.add(new StatusFieldAbility(statuses.theraStatus, 180, 60, 80));
 });
